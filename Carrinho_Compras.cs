8394f872f645aeb74cd9cae8d8385f282fcfcb54
RenanCostaa <renancosta@ucl.br>
2020-10-24 20:18:58 +0000

    Classes feitas a nivel inicial

--- Carrinho_Compras.cs
+++ Carrinho_Compras.cs
@@ -11,12 +11,27 @@ class Carrinho_Compras{
   public string getItens(){
     return itens;
   }
-  public string getQuant(){
+  public int getQuant(){
     return quant_carrinho;
   }
 
   //setters
-  
+  void setDono(string d){
+    dono=d;
+  }
+  void setItens(string i){
+    itens=i;
+  }
+  void setQuant(int q){
+    quant_carrinho=q;
+  }
+
   //contrutores
+  public Carrinho_Compras(string d, string i, int q){
+    dono=d;
+    itens=i;
+    quant_carrinho=q;
+  }
+
   //metodos
 }
